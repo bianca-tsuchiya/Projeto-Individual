4ac8201215882c2ed89ef068c51185e891aa0a2d
bianca-tsuchiya <bianca.tsuchiya@sptech.school>
2022-11-16 19:16:34 -0300

    Subindo alterações no site, início da modelagem e script do Banco de Dados

---
+++ Banco_de_Dados/bd-realistic.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE BD_Realistic;
+USE BD_Realistic;
+
+DROP TABLE Contato;
+
+CREATE TABLE Contato(
+	idContato INT PRIMARY KEY auto_increment,
+    nome VARCHAR(45),
+    sobrenome VARCHAR(45),
+    email VARCHAR(45),
+    assunto VARCHAR(60),
+    mensagem VARCHAR(500)
+);
+
+CREATE TABLE Usuario(
+	idUsuario INT PRIMARY KEY auto_increment,
+	nome VARCHAR(45),
+    sobrenome VARCHAR(45),
+    nome_user VARCHAR(45),
+    data_nasc date,
+    email VARCHAR(45),
+    senha VARCHAR(20)
+);
