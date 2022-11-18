f4e704c2e6030adc2ddf28b890a74caec0f275ce
bianca-tsuchiya <bianca.tsuchiya@sptech.school>
2022-11-18 08:23:27 -0300

    Subindo validações de campos na tela de cadastrado - campos nome e sobrenome, email, data de nascimento e senha

--- Banco_de_Dados/bd-realistic.sql
+++ Banco_de_Dados/bd-realistic.sql
@@ -14,7 +14,7 @@ CREATE TABLE Usuario(
 	idUsuario INT PRIMARY KEY auto_increment,
 	nome VARCHAR(45),
     sobrenome VARCHAR(45),
-    nome_user VARCHAR(45),
+    username VARCHAR(45),
     data_nasc date,
     email VARCHAR(45),
     senha VARCHAR(20)
