6028990ee62a2c18adc7d94b86ccf466f1f4740b
bianca-tsuchiya <bianca.tsuchiya@sptech.school>
2022-11-19 16:22:29 -0300

    Subindo ajustes nas validações da tela de cadastro e integração com o banco de dados

---
+++ Site/src/controllers/usuarioController.js
@@ -0,0 +1,49 @@
+var usuarioModel = require("../models/usuarioModel");
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var nome = req.body.nomeServer;
+    var sobrenome = req.body.sobrenomeServer;
+    var username = req.body.usernameServer;
+    var dataNasc = req.body.dataNascServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (sobrenome == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (username == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else if (dataNasc == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    }
+    else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(nome, sobrenome, username, dataNasc, email, senha)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    cadastrar
+}
